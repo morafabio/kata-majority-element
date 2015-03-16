a26f7056ef751efc9acb58ac8716dfe808a18a11
Fabio Mora <mail@fabiomora.com>
2015-03-17 00:08:07 +0100

    first working solution

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,16 @@ using std::map;
 class Solution {
     public:
         int majorityElement(vector<int> &num) {
+            int halfSize = num.size() / 2;
             map<int, int> votesMap;
 
             for(vector<int>::iterator it = num.begin(); it != num.end(); ++it) {
-                 
+                if( (++votesMap[*it]) > halfSize) {
+                    return *it;
+                }
             } 
-
-            return 42;
+            
+            return -1;
         }
 };
 
