4f2a8db42abaa6590914a03302336e40e8233dfc
Fabio Mora <mail@fabiomora.com>
2015-03-18 00:47:20 +0100

    using boyer-moore majority vote algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,28 @@ using std::map;
 class Solution {
     public:
         int majorityElement(vector<int> &num) {
-            int halfSize = num.size() / 2;
-            map<int, int> votesMap;
+            // initially the candidate is unknown and with 0 votes
+            int votes = 0;
+            int candidate = -1;
 
-            for(vector<int>::iterator it = num.begin(); it != num.end(); ++it) {
-                if( (++votesMap[*it]) > halfSize) {
-                    return *it;
+            for(vector<int>::iterator it = num.begin(); it != num.end(); ++it) 
+            {
+                // not votes, yet? set the new candidate and assign 1 vote
+                if(votes == 0) {
+                    candidate = *it;
+                    votes++;
+                } else {
+                    // increment votes if the current candidate is matching
+                    if(candidate == *it) {
+                        votes++;
+                    // decrement votest if the current candidate is different
+                    } else {
+                        votes--;
+                    }
                 }
-            } 
-            
-            return -1;
+            }
+
+            return candidate; 
         }
 };
 
